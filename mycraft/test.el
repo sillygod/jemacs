80d55124a63f7a9790af67e9199bdd3c8e2605fe
sillygod <sillygod@livemail.tw>
2022-03-22 23:23:21 +0800

    prune spacemacs layers and configs

--- mycraft/test.el
+++
@@ -1,2 +0,0 @@
-(setq today (org-date-to-gregorian
-           (time-to-days (current-time))))
--- myemacs/keybindings.el
+++
@@ -1,9 +0,0 @@
-(with-eval-after-load 'exercism
-  (evil-define-key 'normal exercism-mode-map (kbd "g s") 'exercism-submit))
-
-;; try to bind the this function with evil key maps
-;; TODO: rethink the key binding
-(evil-define-key 'visual 'global
-  (kbd "g y") 'copy-region-and-base64-decode
-  (kbd "g e") 'copy-region-and-urlencode)
-
