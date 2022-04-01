dd6a265493f7622261886ccbc95cd40246d026b7
sillygod <sillygod@livemail.tw>
2022-04-01 23:32:22 +0800

    start to structurize personal emacs configs

---
+++ local/k8s/k8s.el
@@ -0,0 +1,32 @@
+;;; k8s.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Jing
+;; Maintainer: Jing
+;; Version: 0.0.1
+;; Keywords: kubernetes
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;;
+;;; Code:
+
+
+
+
+(provide 'k8s)
+
+;;; k8s.el ends here
